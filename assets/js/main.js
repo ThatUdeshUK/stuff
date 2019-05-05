fbc1534aab2d50e3055bef14785497530c54ca10
Udesh Kumarasinghe <mail@udeshuk.me>
2019-05-05 11:57:44 +0530

    Implemented sidebar toggle

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function openSidebar() {
+  // $('.sidebar').animate({ left: 0 })
+  $('.sidebar').removeClass('sidebar-collapse')
+}
+
+function collapseSidebar() {
+  $('.sidebar').addClass('sidebar-collapse')
+}
